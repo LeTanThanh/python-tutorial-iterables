9d5046c2fd580ca5292d0775a21d0d01c09d62d3
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 22:01:20 +0700

    Introduction to Python iterables

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 if __name__ == "__main__":
-  print("Python Iterables")
+  # Introduction to Python iterables
+
+  for index in range(3):
+    print(index)
+
+  str = "Iterables"
+  for ch in str:
+    print(ch)
+
+  ranks = ["high", "medium", "low"]
+  for rank in ranks:
+    print(rank)
