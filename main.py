d4999dd7490110fe543608c10eb8815dac920b89
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 22:03:43 +0700

    What is an iterator

--- main.py
+++ main.py
@@ -11,3 +11,28 @@ if __name__ == "__main__":
   ranks = ["high", "medium", "low"]
   for rank in ranks:
     print(rank)
+
+  # What is an iterator
+
+  colors = ["red", "green", "blue"]
+  print(colors)
+
+  iter_colors = iter(colors)
+  print(iter_colors)
+
+  color = next(iter_colors)
+  print(color)
+
+  color = next(iter_colors)
+  print(color)
+
+  color = next(iter_colors)
+  print(color)
+
+  # color = next(iter_colors)
+  # StopIteration
+
+  colors = ["red", "green", "blue"]
+  iter_colors = iter(colors)
+  for color in iter_colors:
+    print(color)
